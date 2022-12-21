04fa49423828a8a0200404b49d74d853842e1b27
PetrikPavel <academ7799@gmail.com>
2022-12-21 18:05:47 +0300

    Add file test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import  datetime, time
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+nachalo = time.time()
+start = datetime.datetime.now()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi(f'Python_4 started in {start.strftime("%c")}')
+    print(f"Это файл: test.py")
+    
