a85383828934a634526b21dcc6413b1dbaec9dba
PetrikPavel <academ7799@gmail.com>
2022-12-21 17:32:07 +0300

    Add project python_4

---
+++ main.py
@@ -0,0 +1,18 @@
+import  datetime, time
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+nachalo = time.time()
+start = datetime.datetime.now()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi(f'Python_4 started in {start.strftime("%c")}')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
